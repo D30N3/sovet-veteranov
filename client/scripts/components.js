b94312e07cb25201053dc042c1d06bb03f9ccbfe
Lex973 <lenya.kurinin@mail.ru>
2025-11-29 13:38:06 +0300

    add all components

---
+++ client/scripts/components.js
@@ -0,0 +1,7 @@
+document.querySelector('.button').addEventListener('click', function() {
+    if (this.classList.contains("active")) {
+        this.classList.remove("active")
+    } else {
+        this.classList.add("active");
+    }
+});
