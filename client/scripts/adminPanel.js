3e6412f270532c4034258ee41c3fc46b571a8cc0
Lex973 <lenya.kurinin@mail.ru>
2025-12-19 14:26:29 +0300

    small changes

--- client/scripts/adminPanel.js
+++ client/scripts/adminPanel.js
@@ -1,29 +1,49 @@
 const hastagBtn = document.getElementById("hastag-btn");
 const hastagList = document.getElementById("hastag-list");
+const hastagListContainer = document.querySelector(".hastag-list-container");
 const arrow = document.querySelector(".arrow-bottom");
 let isOpen = false;
 
-hastagBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!isOpen) {  
-        if (window.innerWidth > 688) {
-            hastagList.style.maxHeight = "200px";
-        }
-        if (window.innerWidth < 688) {
-            hastagList.style.maxHeight = "90px";
-        }
-        hastagList.style.opacity = "1";
-        hastagList.style.marginTop = "20px"
-        hastagList.style.pointerEvents = "all";
-        arrow.classList.add("rotate");
-        arrow.classList.remove("unrotate");
-    } else {
-        hastagList.style.maxHeight = "0";
-        hastagList.style.opacity = "0";
-        hastagList.style.marginTop = "0"
-        hastagList.style.pointerEvents = "none";
-        arrow.classList.remove("rotate");
-        arrow.classList.add("unrotate");
-    }
-    isOpen = !isOpen;
+hastagBtn.addEventListener("click", (e) => {
+e.preventDefault();
+if (!isOpen) {
+    hastagList.style.maxHeight = "500px";
+    hastagList.style.opacity = "1";
+    hastagList.style.visibility = "visible";
+    hastagList.style.pointerEvents = "all";
+    
+    hastagListContainer.classList.add("show");
+    
+    arrow.classList.add("rotate");
+    arrow.classList.remove("unrotate");
+} else {
+    hastagList.style.maxHeight = "0";
+    hastagList.style.opacity = "0";
+    hastagList.style.visibility = "hidden";
+    hastagList.style.pointerEvents = "none";
+    
+    hastagListContainer.classList.remove("show");
+    
+    arrow.classList.remove("rotate");
+    arrow.classList.add("unrotate");
+}
+
+isOpen = !isOpen;
+
+});
+
+document.addEventListener("click", (e) => {
+if (isOpen && !hastagBtn.contains(e.target) && !hastagListContainer.contains(e.target)) {
+hastagList.style.maxHeight = "0";
+hastagList.style.opacity = "0";
+hastagList.style.visibility = "hidden";
+hastagList.style.pointerEvents = "none";
+    hastagListContainer.classList.remove("show");
+    
+    arrow.classList.remove("rotate");
+    arrow.classList.add("unrotate");
+    
+    isOpen = false;
+}
+
 });
